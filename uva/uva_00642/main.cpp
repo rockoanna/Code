bd386e9621dd3a767743fe74f533a8d23d97faec
Oanna <oanna.pasca@gmail.com>
2019-09-05 09:56:55 +0300

    uva 642

--- uva/uva_00642/main.cpp
+++ uva/uva_00642/main.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+#include <fstream>
 
 using namespace std;
 
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //ofstream cout {"out.txt"};
+    unordered_map<string, set<string>> d;
+    string s;
+    while(true)
+    {
+        cin >> s;
+        if(s == "XXXXXX")
+        {
+            break;
+        }
+        string s_c(s);
+        sort(s_c.begin(),s_c.end());
+        auto &res = d[s_c];
+        res.insert(s);
+    }
+//    for(auto kv : d)
+//    {
+//        cout << kv.first <<" { " ;
+//        auto words = kv.second;
+//        for(auto w: words)
+//        {
+//            cout << w << '\n';
+//        }
+//        cout <<"}"<<endl;
+//
+//    }
+    string word;
+    while(true)
+    {
+        cin >> word;
+        if(word == "XXXXXX")
+        {
+            break;
+        }
+        sort(word.begin(), word.end());
+        auto res = d.find(word);
+        if(res != d.end())
+        {
+            auto words = (*res).second;
+            for(auto w: words)
+            {
+                cout << w << '\n';
+            }
+
+        }
+        else
+        {
+            cout << "NOT A VALID WORD" << '\n';
+        }
+        cout << "******" << '\n';
+    }
     return 0;
 }
